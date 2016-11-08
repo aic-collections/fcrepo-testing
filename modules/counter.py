26d4bcf0ded8fed526cf3d141719de04b777d192
Kevin Ford <kefo@3windmills.com>
2016-11-08 09:07:09 -0600

    Support loading of binary content.

--- modules/counter.py
+++ modules/counter.py
@@ -13,14 +13,23 @@ class Counter:
         self.total_5xxs = 0
         self.total_unanticipated_responses = 0
         self.avgtime_sum = False
+        
+        self.total_processed_binary = 0
+        self.total_unprocessed_binary = 0
+        self.total_errors_binary = 0
+        self.total_httperrors_binary = 0
     
-    def output(self, starttime, endtime, timedelta):
+    def output(self, starttime, endtime, timedelta, load_binary_data):
         print ()
         print ("Task started at: " + starttime)
         print ("Task ended at: " + endtime)
         print ("Elapsed time: ", str(timedelta))
         print ("Number of resources processed: ", str(self.total_processed))
         print ("Number of resources unprocessed: ", str(self.total_unprocessed))
+        if load_binary_data:
+            print ("Number of binaries processed: ", str(self.total_processed_binary))
+            print ("Number of binaries unprocessed: ", str(self.total_unprocessed_binary))
+            print ("Number of HTTP errors for binary: ", str(self.total_httperrors_binary))
         print ("Number of HTTP errors: ", str(self.total_httperrors))
         print ("Number of 200s: ", str(self.total_200s))
         print ("Number of 201s: ", str(self.total_201s))
@@ -30,7 +39,7 @@ class Counter:
         print ("Number of unanticipated responses: ", str(self.total_unanticipated_responses))
         print()
 
-    def update(self, i):
+    def update(self, i, load_binary_data):
         if self.avgtime_sum:
             self.avgtime_sum = self.avgtime_sum + i["timedelta"]
         else:
@@ -55,3 +64,29 @@ class Counter:
             self.total_5xxs = self.total_5xxs + 1
         else:
             self.total_unanticipated_responses = self.total_unanticipated_responses + 1
+        
+        if load_binary_data:
+            if self.avgtime_sum:
+                self.avgtime_sum = self.avgtime_sum + i["timedelta_binary"]
+            else:
+                self.avgtime_sum = i["timedelta_binary"]
+        
+            if i["processed_binary"] == 1:
+                self.total_processed_binary = self.total_processed_binary + 1
+            else:
+                self.total_unprocessed_binary = self.total_unprocessed_binary + 1
+        
+            if i["httperror"] == 1:
+                self.total_httperrors_binary = self.total_httperrors_binary + 1
+            if i["response_status_code_binary"] == 200:
+                self.total_200s = self.total_200s + 1
+            elif i["response_status_code_binary"] == 201:
+                self.total_201s = self.total_201s + 1
+            elif i["response_status_code_binary"] == 204:
+                self.total_204s = self.total_204s + 1
+            elif i["response_status_code_binary"] > 399 and i["response_status_code_binary"] < 500:
+                self.total_4xxs = self.total_4xxs + 1
+            elif i["response_status_code_binary"] > 499:
+                self.total_5xxs = self.total_5xxs + 1
+            else:
+                self.total_unanticipated_responses = self.total_unanticipated_responses + 1
